8731165bb3b2f9e4c97e39218378a660e008d6aa
AlyssaHarrower <128412406+AlyssaHarrower@users.noreply.github.com>
2023-11-23 14:50:19 -0500

    updated signature_generation.py and added test .exe files

--- signature_generation.py
+++ signature_generation.py
@@ -1,21 +1,24 @@
 from difflib import SequenceMatcher #importing comparison library
 
 #opens target 'malicious' file and reads it
-#f = open('module1.exe', 'b+r')
-#filebin = f.read()
-#f.close()
+f = open('test1.exe', 'b+r')
+filedata = str(f.read())
+f.close()
 
 #opens target 'malicious' file and reads it
-#r = open('module2.exe', 'b+r')
-#filebin2 = r.read()
-#r.close()
+r = open('test2.exe', 'b+r')
+filedata2 = str(r.read())
+r.close()
+
+filebin = ''.join(format(ord(i), '08b') for i in filedata)
+filebin2 = ''.join(format(ord(i), '08b') for i in filedata2)
 
 #stand-in to test program
-test1 = '01111011110101'
-test2 = '0111100000101010'
+#test1 = '01111011110101'
+#test2 = '0111100000101010'
 
-match = SequenceMatcher(None,test1 ,test2).find_longest_match()
+#match = SequenceMatcher(None,filebin,filebin2).find_longest_match()
 
-print(match)  # -> Match(a=0, b=15, size=9)
-print(test1[match.a:match.a + match.size])  # -> 011110
-print(test2[match.b:match.b + match.size])  # -> 011110
+#print(match)  # -> Match(a=0, b=15, size=9)
+#print(filebin[match.a:match.a + match.size])  # -> 011110
+#print(filebin2[match.b:match.b + match.size])  # -> 011110
